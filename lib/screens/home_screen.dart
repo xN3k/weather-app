4a518b97f18e73593c62318bcef7e90e7ed555fa
xN3k <aadarshchaudhary463@gmail.com>
2024-08-30 23:10:17 +0545

    fixed display overflow

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -16,50 +16,78 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
     final weatherProvider = Provider.of<WeatherProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('WeatherApp'),
         actions: [
           IconButton(
             onPressed: themeProvider.toggleTheme,
-            icon: themeProvider.isDarkTheme
-                ? const Icon(Icons.light_mode_outlined)
-                : const Icon(Icons.dark_mode_outlined),
+            icon: Icon(
+              themeProvider.isDarkTheme
+                  ? Icons.light_mode_outlined
+                  : Icons.dark_mode_outlined,
+            ),
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(16),
-            child: TextField(
-              decoration: const InputDecoration(
-                labelText: 'Search City',
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.search),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          // Wrap Column in SingleChildScrollView
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Search City',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.search),
+                ),
+                onSubmitted: (value) {
+                  if (value.isNotEmpty) {
+                    weatherProvider.fetchWeather(value);
+                  }
+                },
               ),
-              onSubmitted: (value) => weatherProvider.fetchWeather(value),
-            ),
+              const SizedBox(height: 40),
+              if (weatherProvider.isLoading)
+                const CircularProgressIndicator()
+              else if (weatherProvider.errorMessage != null)
+                Text(
+                  weatherProvider.errorMessage!,
+                  style: Theme.of(context).textTheme.titleLarge,
+                  textAlign: TextAlign.center,
+                )
+              else if (weatherProvider.weather != null)
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      weatherProvider.weather!.city,
+                      style: Theme.of(context).textTheme.displayLarge,
+                    ),
+                    const SizedBox(height: 20),
+                    Lottie.asset('assets/animations/sunny.json'),
+                    const SizedBox(height: 40),
+                    Text(
+                      '${weatherProvider.weather!.temperature}°C',
+                      style: Theme.of(context).textTheme.headlineLarge,
+                    ),
+                    Text(
+                      'Weather: ${weatherProvider.weather!.condition}',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                  ],
+                )
+              else
+                Text(
+                  'No City Selected',
+                  style: Theme.of(context).textTheme.displaySmall,
+                ),
+            ],
           ),
-          const SizedBox(
-            height: 10,
-          ),
-          if (weatherProvider.isLoading)
-            const CircularProgressIndicator()
-          else if (weatherProvider.errorMessage != null)
-            Text(weatherProvider.errorMessage!)
-          else if (weatherProvider.weather != null)
-            Column(
-              children: [
-                Text(weatherProvider.weather!.city),
-                Lottie.asset('assets/animations/sunny.json'),
-                Text('Temperature: ${weatherProvider.weather!.temperature}°C'),
-                Text('Weather: ${weatherProvider.weather!.condition}'),
-              ],
-            )
-          else
-            Text('Enter a city to get weather data'),
-        ],
+        ),
       ),
     );
   }
